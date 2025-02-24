c1cf51a52f4f822d8e1ac149b559c534bd95033d
dev003 <team.bta.003@bamboo-tech-asia.com>
2025-02-24 15:14:38 +0800

    fix: syntax error

--- plugins/history_plugin/history_plugin.cpp
+++ plugins/history_plugin/history_plugin.cpp
@@ -228,6 +228,7 @@ namespace upcx {
               aho.account = n;
               aho.action_sequence_num = act.receipt->global_sequence;
               aho.account_sequence_num = asn;
+              aho.trx_id = act.trx_id;
             });
          }
 
